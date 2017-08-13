7acb03c64b6a2f28168d66df4da7c192acd5cd8a
Clarke Rahrig <crahrig@gmail.com>
2017-08-13 12:06:43 -0700

    Approriate environment variables set to run locally

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,7 +1,9 @@
 @echo off
 
-call npm install
-SETLOCAL
-SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+npm install && node_modules\.bin\hubot.cmd --name "bender" %*
 
-node_modules\.bin\hubot.cmd --name "flamebot" %* 
+rem call npm install
+rem SETLOCAL
+rem SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
+
+rem node_modules\.bin\hubot.cmd --name "flamebot" %* 
