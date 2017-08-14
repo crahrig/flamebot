343791160042d277118cdb4167622dab31c0cc02
Clarke Rahrig <crahrig@gmail.com>
2017-08-13 17:52:35 -0700

    Changed hubot to flamebot

--- deploy.sh
+++ deploy.sh
@@ -118,7 +118,7 @@ if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
 fi
 
 # 4. Create Hubot file with coffee extension
-cp "$DEPLOYMENT_TARGET/node_modules/hubot/bin/hubot" "$DEPLOYMENT_TARGET/node_modules/hubot/bin/hubot.coffee"
+cp "$DEPLOYMENT_TARGET/node_modules/flamebot/bin/hubot" "$DEPLOYMENT_TARGET/node_modules/flamebot/bin/hubot.coffee"
 
 ##################################################################################################################################
 echo "Finished successfully."
