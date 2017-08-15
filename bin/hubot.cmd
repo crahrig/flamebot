669ea59399dd9103d8e27c400daea8f4d409dde0
Clarke Rahrig <crahrig@gmail.com>
2017-08-14 21:45:36 -0700

    Removed a commented out line

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -1,7 +1,5 @@
 @echo off
 
-rem epm install && node_modules\.bin\hubot.cmd --name "flamebot" %*
-
 call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
